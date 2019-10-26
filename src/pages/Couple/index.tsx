fe48b711f164bae7d9b088ec2ad4901c23d716d0
Artem Kravchenko <kravchenkofirst@gmail.com>
2019-10-26 18:40:35 +0700

    feat: add the device block to the couple page

--- src/pages/Couple/index.tsx
+++ src/pages/Couple/index.tsx
@@ -1,11 +1,48 @@
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import faker from 'faker';
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 
+// components
+import Grid from '@material-ui/core/Grid';
+
+import Device from './widgets/Device';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    block: {
+      flexGrow: 1,
+    },
+  }),
+);
+
+const DEVICE = {
+  device_id: faker.random.alphaNumeric(8),
+  protocol: faker.system.semver(),
+  firmware: faker.system.semver(),
+  device_type: 'Parking sensor',
+  activation_status: faker.random.boolean(),
+  damaged_status: faker.random.boolean(),
+  encryption_keys: [faker.random.uuid()],
+  owner: faker.company.companyName(),
+};
+
 const CouplePage: React.FC = () => {
+  const layoutCss = useStyles();
+
   return (
-    <Helmet>
-      <title>Couple</title>
-    </Helmet>
+    <>
+      <Helmet>
+        <title>Couple</title>
+      </Helmet>
+
+      <Grid container spacing={2}>
+        <Grid className={layoutCss.block} item>
+          <Device device={DEVICE} idPrefix="device" />
+        </Grid>
+        <Grid className={layoutCss.block} item />
+      </Grid>
+    </>
   );
 };
 

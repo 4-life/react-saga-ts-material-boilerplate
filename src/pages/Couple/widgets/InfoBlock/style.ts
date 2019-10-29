92799be5418f95368942aadb5a0e8d6e098915bf
Artem Kravchenko <kravchenkofirst@gmail.com>
2019-10-29 18:30:24 +0700

    feat: implement the skeleton of place binding block on couple page

--- src/pages/Couple/widgets/InfoBlock/style.ts
+++ src/pages/Couple/widgets/InfoBlock/style.ts
@@ -2,6 +2,9 @@ import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 
 export const useInfoBlockStyles = makeStyles((theme: Theme) =>
   createStyles({
+    actions: {
+      alignSelf: 'flex-end',
+    },
     button: {
       margin: theme.spacing(1),
     },
--- src/pages/Couple/widgets/Place/utils.ts
+++ src/pages/Couple/widgets/Place/utils.ts
@@ -2,6 +2,7 @@ const __PlaceFormFieldNames = {
   GEO_LAT: 'lat',
   GROUP: 'group_id',
   BATCH: 'batch_id',
+  ID: 'id',
 };
 
 export const PlaceFormFieldNames: Readonly<typeof __PlaceFormFieldNames> = (

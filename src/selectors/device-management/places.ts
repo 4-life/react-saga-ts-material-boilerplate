b5af64893e612a4ab8c306dc9c7c8b5106fd898b
Artem Kravchenko <kravchenkofirst@gmail.com>
2019-10-30 00:04:30 +0700

    feat: provide selectors for the places reducer

---
+++ src/selectors/device-management/places.ts
@@ -0,0 +1,19 @@
+import { Place } from '../../models';
+import { RootState as State } from '../../reducers';
+
+import { name as DEVICE_MANAGEMENT_REDUCER_NAME } from '../../reducers/device-management';
+import { name as PLACES_REDUCER_NAME } from '../../reducers/device-management/places';
+
+function getPlacesState(state: State) {
+  return state[DEVICE_MANAGEMENT_REDUCER_NAME][PLACES_REDUCER_NAME];
+}
+
+export function arePlacesLoading(state: State) {
+  const placesState = getPlacesState(state);
+  return placesState.isFetching;
+}
+
+export function getPlace(placeId: Place['id'], state: State) {
+  const placesState = getPlacesState(state);
+  return placesState.places.find(place => place.id === placeId);
+}

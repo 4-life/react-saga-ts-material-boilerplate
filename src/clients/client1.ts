a29b4c936344f758f85409e58a1cf4c7e503dc57
Artem Kravchenko <kravchenkofirst@gmail.com>
2019-10-31 14:10:56 +0700

    feat: fill received partial device objects with arbitrary data

--- src/clients/client1.ts
+++ src/clients/client1.ts
@@ -1,3 +1,5 @@
+import faker from 'faker';
+
 import { FetchDevices } from '../actions/device-management/devices';
 import { ApiResponse, Device, Place } from '../models';
 import { GetPlaces } from '../actions/dummy-data';
@@ -16,9 +18,45 @@ const urls = {
 
 export type FetchDevicesResponse = ApiResponse<Array<Device>>;
 
+function fillDevice(device: Partial<Device>): Device {
+  // FIXME: use smth like `defaults` from "lodash" instead
+  // to prevent filling the final object with nil values
+  // if the original `device` suddenly contains them
+  return {
+    device_id: faker.random.alphaNumeric(8),
+    protocol: faker.system.semver(),
+    firmware: faker.system.semver(),
+    device_type: 'Parking sensor',
+    activation_status: faker.random.boolean(),
+    damaged_status: faker.random.boolean(),
+    encryption_keys: [faker.random.uuid()],
+    owner: faker.company.companyName(),
+  
+    last_update_time: faker.date.past().toISOString(),
+    last_firmware_update_time: faker.date.past().toISOString(),
+    production_batch_id: faker.random.number(),
+    assembling_time: faker.date.past().toISOString(),
+    purchase_time: faker.date.past().toISOString(),
+    warranty_expiration_time: faker.date.past().toISOString(),
+    last_disabling_time: faker.date.past().toISOString(),
+    last_disabling_reason: faker.lorem.words(20),
+
+    // monitoring information
+    last_received_message_time: faker.date.past().toISOString(),
+    number_of_open_incidents: faker.random.number({ min: 0, max: 10, precision: 1 }),
+    current_connectivity: faker.random.number({ min: 0, max: 100, precision: 1 }),
+
+    ...device
+  };
+}
+
 export const fetchDevices = (action: FetchDevices): Promise<FetchDevicesResponse> => {
   return fetch(urls.findDevices)
     .then((res) => res.json())
+    .then((res: ApiResponse<Array<Partial<Device>>>) => ({
+      ...res,
+      data: res.data && res.data.map(fillDevice),
+    }))
     .catch((err) => err);
 };
 

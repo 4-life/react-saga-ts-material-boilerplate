eb18512930e94e24e65edf3cf30a1eaa0755d367
Artem Kravchenko <kravchenkofirst@gmail.com>
2019-11-03 19:08:49 +0700

    feat: add support of rendering breadcrumbs as links

--- src/components/Header/utils.tsx
+++ src/components/Header/utils.tsx
@@ -5,7 +5,10 @@ import Routes from '../../Routes';
 import { Route } from '../../routing/typings';
 
 // components
+import MuiLink from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
+
+import Link from '../../components/Link';
 
 export function renderBreadcrumbsEntries({ pathname }) {
   const branch = matchRoutes(Routes, pathname);
@@ -23,11 +26,42 @@ export function renderBreadcrumbsEntries({ pathname }) {
       );
 
       if (crumbContent) {
-        breadcrumbs.push(
-          <Typography key={i} variant="h6" color="textSecondary">
-            {crumbContent}
-          </Typography>
+        let crumbNode;
+
+        type CommonProps = {
+          children: React.ReactNode,
+          color: 'textSecondary',
+          key: number,
+          variant: 'h6'
+        };
+
+        const commonProps: CommonProps = {
+          children: crumbContent,
+          color: 'textSecondary',
+          key: i,
+          variant: 'h6',
+        };
+
+        const showAsLink = (
+          !matchedRoute.match.isExact &&
+          // TODO: support route-specific dynamic determination
+          // of this depending on match or any other parameters?
+          !matchedRoute.route.nonLinkBreadcrumb
         );
+
+        if (showAsLink) {
+          crumbNode = (
+            <MuiLink
+              {...commonProps}
+              component={Link}
+              to={matchedRoute.match.url}
+            />
+          );
+        } else {
+          crumbNode = <Typography {...commonProps} />;
+        }
+
+        breadcrumbs.push(crumbNode);
       }
 
       return breadcrumbs;
